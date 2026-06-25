74cc59ff880e522f66bd0e01e5517a7f4b17cb31
MDC-Cl-PC18 <aprilmayayta.com>
2026-06-25 10:13:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Sample from './components/Sample.vue'
+</script>
+
+<template>
+  <HelloWorld />
+  <Sample/>
+</template>
---
+++ src/components/Sample.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './HelloWorld.vue';
+
+</script>
+<template>
+    <div>
+        <h1>buang ka</h1>
+    </div>
+</template>
